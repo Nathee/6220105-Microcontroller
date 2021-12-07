838114414f6563cf8c5e4ac87da1cd9a33af1f93
Nathee <s_nathee@live.com>
2021-12-07 10:43:14 +0700

    update

--- if-else-if-else/if-else-fi-else.ino
+++ if-else-if-else/if-else-fi-else.ino
@@ -1,9 +1,11 @@
-string weekDay ="Tuesday";
-
+string weekDay = "Tuesday";
+string message = "";
 void setup() {
   Serial.begin(115200);
 }
 
 void loop() {
-    
+    if(weekDay=="Monday"){
+
+    }
 }
