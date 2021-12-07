1ded5bb07112e114c4c383cd93b333b9c260219f
Nathee <s_nathee@live.com>
2021-12-07 10:27:45 +0700

    creare voidLoop

---
+++ voidLoop/voidLoop.ino
@@ -0,0 +1,14 @@
+int number = 1;
+float wieght = 0.9;
+
+void setup()
+{
+    Serial.begin(115200);
+    Serial.println("Nooknickzzi");
+}
+
+void loop()
+{
+    Serial.println("WE-ARE-EN-LIC");
+    delay(1000);
+}
