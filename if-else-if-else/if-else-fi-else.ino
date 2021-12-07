097b5301222e89d4e62156f3581f090f98eabd62
Nathee <s_nathee@live.com>
2021-12-07 10:47:35 +0700

    update

--- if-else-if-else/if-else-fi-else.ino
+++ if-else-if-else/if-else-fi-else.ino
@@ -1,11 +1,22 @@
 string weekDay = "Tuesday";
 string message = "";
-void setup() {
-  Serial.begin(115200);
+void setup()
+{
+    Serial.begin(115200);
 }
 
-void loop() {
-    if(weekDay=="Monday"){
-
+void loop()
+{
+    if (weekDay == "Monday")
+    {
+        message = "เรียนวิชาภาษาอังกฤษ";
+    }
+    else if (weekDay == "Tuesday")
+    {
+        message = "เรียนวิชาไมโครคอนโทรลเลอร์";
+    }
+    else
+    {
+        message = "วันหยุดเสาร์-อาทิตย์";
     }
 }
